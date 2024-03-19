aac5b18b3a8c7ccd0e7e5918978d1bb9df3ca033
Arman-Markaryan <139840328+Arman-Markaryan@users.noreply.github.com>
2024-03-18 19:55:43 -0700

    Update dice.c

--- Dice Rolling Game/dice.c
+++ Dice Rolling Game/dice.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
-
 
 int main() {
     int dice;
-    char command[10];
-
+    char input[10];
 
     srand(time(0));
-    dice = rand() % 6 + 1;
-
 
     printf("Welcome to Dice Rollers!\n");
-    printf("Type 'roll' to roll the dice: ");
 
+    while (1) {
+        printf("Type 'roll' to roll your dice: ");
+        scanf("%s", input);
 
-    scanf("%s", command);
-
-     if (strcmp(command, "roll") == 0) {
-        printf("You rolled: %d\n", dice);
-    } else {
-        printf("Invalid command. Type 'roll' to roll the dice.\n");
+        if (strcmp(input, "roll") == 0) {
+            dice = rand() % 6 + 1;
+            printf("You rolled a: %d\n", dice);
+            break; 
+        } else {
+            printf("Invalid command. Please type 'roll' to roll the dice.\n");
+        }
     }
-
-
 
     return 0;
 }
